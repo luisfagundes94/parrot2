c3f60d8a7bbb69db27db2ed29cccfb421ea84831
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-07-12 16:58:40 -0300

    Add translation results component

---
+++ feature/translation/src/main/java/com/luisfagundes/translation/presentation/components/TranslationResults.kt
@@ -0,0 +1,44 @@
+package com.luisfagundes.translation.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import com.luisfagundes.designsystem.theme.spacing
+import com.luisfagundes.translation.presentation.TranslationUiState
+
+@Composable
+internal fun TranslationResults(
+    uiState: TranslationUiState,
+    textStyle: TextStyle,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.default),
+        modifier = modifier
+    ) {
+        if (uiState.isLoading) {
+            CircularProgressIndicator()
+        }
+
+        if (uiState.errorMessage.isNotBlank()) {
+            Text(
+                text = uiState.errorMessage,
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+        if (uiState.translatedText.isNotBlank()) {
+            Text(
+                text = uiState.translatedText,
+                style = textStyle,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
+}
