63d60eb4b873b5a636f150b9deb24c073dd7d7e2
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-07-12 16:58:13 -0300

    Add custom translation input field

---
+++ feature/translation/src/main/java/com/luisfagundes/translation/presentation/components/TranslationInputField.kt
@@ -0,0 +1,41 @@
+package com.luisfagundes.translation.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import com.luisfagundes.translation.R
+
+@Composable
+internal fun TranslationInputField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    textStyle: TextStyle,
+    modifier: Modifier = Modifier
+) {
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        textStyle = textStyle.copy(color = MaterialTheme.colorScheme.onSurface),
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+        modifier = modifier,
+        decorationBox = { innerTextField ->
+            Box(modifier = Modifier.fillMaxWidth()) {
+                if (value.isEmpty()) {
+                    Text(
+                        text = stringResource(R.string.enter_text),
+                        style = textStyle,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+                    )
+                }
+                innerTextField()
+            }
+        }
+    )
+}
