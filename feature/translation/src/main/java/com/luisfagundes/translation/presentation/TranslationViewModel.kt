5ae929bc0d40aa194e67f244e73731fbd4c4234e
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-07-12 15:02:09 -0300

    Add TranslationViewModel.kt

---
+++ feature/translation/src/main/java/com/luisfagundes/translation/presentation/TranslationViewModel.kt
@@ -0,0 +1,52 @@
+package com.luisfagundes.translation.presentation
+
+import androidx.lifecycle.viewModelScope
+import com.luisfagundes.common.dispatcher.AppDispatcher
+import com.luisfagundes.common.dispatcher.Dispatcher
+import com.luisfagundes.common.presentation.ViewModel
+import com.luisfagundes.translation.domain.model.Translation
+import com.luisfagundes.translation.domain.model.TranslationParams
+import com.luisfagundes.translation.domain.usecase.GetSupportedLanguageListUseCase
+import com.luisfagundes.translation.domain.usecase.TranslateTextUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TranslationViewModel @Inject constructor(
+    private val getSupportedLanguageListUseCase: GetSupportedLanguageListUseCase,
+    private val translateTextUseCase: TranslateTextUseCase,
+    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : ViewModel<TranslationUiState>(
+    initialState = TranslationUiState()
+) {
+    fun translateText(text: String, targetLang: String) = viewModelScope.launch {
+        val params = TranslationParams(
+            text = listOf(text),
+            targetLanguage = targetLang
+        )
+        translateTextUseCase.invoke(params)
+            .flowOn(coroutineDispatcher)
+            .onStart { showLoading() }
+            .catch { throwable -> showError(throwable) }
+            .collect { translationList -> showResult(translationList) }
+    }
+
+    private fun showLoading() {
+        updateState { state -> state.setLoading(true) }
+    }
+
+    private fun showError(throwable: Throwable) {
+        val message = throwable.localizedMessage ?: throwable.toString()
+        updateState { state -> state.setError(message) }
+    }
+
+    private fun showResult(translationList: List<Translation>) {
+        updateState { state -> state.setResult(translationList.toString()) }
+    }
+}
