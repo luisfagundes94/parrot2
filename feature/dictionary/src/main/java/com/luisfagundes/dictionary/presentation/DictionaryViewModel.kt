69bd58c4e03ed0400b37f34b638ffee61c2d3aa7
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-07-14 19:30:28 -0300

    Add error message when word list is empty

--- feature/dictionary/src/main/java/com/luisfagundes/dictionary/presentation/DictionaryViewModel.kt
+++ feature/dictionary/src/main/java/com/luisfagundes/dictionary/presentation/DictionaryViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.viewModelScope
 import com.luisfagundes.common.dispatcher.AppDispatcher.IO
 import com.luisfagundes.common.dispatcher.Dispatcher
 import com.luisfagundes.common.presentation.ViewModel
+import com.luisfagundes.common.provider.ResourceProvider
+import com.luisfagundes.dictionary.R
 import com.luisfagundes.dictionary.domain.model.TranslationParams
 import com.luisfagundes.dictionary.domain.model.Word
 import com.luisfagundes.dictionary.domain.usecase.GetTranslationsUseCase
@@ -18,8 +20,9 @@ import javax.inject.Inject
 @HiltViewModel
 internal class DictionaryViewModel @Inject constructor(
     private val getTranslationsUseCase: GetTranslationsUseCase,
+    private val resourceProvider: ResourceProvider,
     @param:Dispatcher(IO) private val dispatcher: CoroutineDispatcher
-): ViewModel<DictionaryUiState>(
+) : ViewModel<DictionaryUiState>(
     initialState = DictionaryUiState()
 ) {
     fun translate(text: String) = viewModelScope.launch {
@@ -31,8 +34,14 @@ internal class DictionaryViewModel @Inject constructor(
         getTranslationsUseCase.invoke(params)
             .flowOn(dispatcher)
             .onStart { setLoadingState() }
-            .catch { throwable -> setErrorState(throwable.toString()) }
-            .collect { words -> setSuccessState(words) }
+            .catch { throwable -> setErrorState(throwable.message.toString()) }
+            .collect { words ->
+                if (words.isEmpty()) {
+                    setErrorState(resourceProvider.getString(R.string.translation_not_found))
+                } else {
+                    setSuccessState(words)
+                }
+            }
     }
 
     private fun setLoadingState() {
