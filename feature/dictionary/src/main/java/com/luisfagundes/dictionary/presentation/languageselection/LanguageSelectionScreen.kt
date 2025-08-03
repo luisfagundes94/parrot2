1cc3c7190fa10a738a44715b6a2ace2f1b88a71f
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-08-03 18:32:29 -0300

    Change language selection item card container color to surfaceContainer

--- feature/dictionary/src/main/java/com/luisfagundes/dictionary/presentation/languageselection/LanguageSelectionScreen.kt
+++ feature/dictionary/src/main/java/com/luisfagundes/dictionary/presentation/languageselection/LanguageSelectionScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -147,7 +148,7 @@ private fun LanguageItem(
             containerColor = if (isSelected) {
                 MaterialTheme.colorScheme.primary
             } else {
-                MaterialTheme.colorScheme.surfaceVariant
+                MaterialTheme.colorScheme.surfaceContainer
             }
         ),
         modifier = modifier.fillMaxWidth()
