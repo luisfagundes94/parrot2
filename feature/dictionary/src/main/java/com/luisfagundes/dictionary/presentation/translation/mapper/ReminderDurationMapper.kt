2e3e9485addd7e6bfa793734e7042d2fcb3c5e15
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2025-08-23 17:02:39 -0300

    Add mapToDomain to ReminderDurationMapper.kt

--- feature/dictionary/src/main/java/com/luisfagundes/dictionary/presentation/translation/mapper/ReminderDurationMapper.kt
+++ feature/dictionary/src/main/java/com/luisfagundes/dictionary/presentation/translation/mapper/ReminderDurationMapper.kt
@@ -1,17 +1,22 @@
 package com.luisfagundes.dictionary.presentation.translation.mapper
 
 import com.luisfagundes.dictionary.domain.model.ReminderDuration
-import com.luisfagundes.dictionary.domain.model.ReminderDuration.ONE_WEEK
-import com.luisfagundes.dictionary.domain.model.ReminderDuration.TWO_WEEKS
-import com.luisfagundes.dictionary.domain.model.ReminderDuration.ONE_MONTH
 import com.luisfagundes.dictionary.presentation.translation.components.UiReminderDuration
 
 internal class ReminderDurationMapper {
     fun ReminderDuration.mapToUi(): UiReminderDuration {
         return when (this) {
-            ONE_WEEK -> UiReminderDuration.ONE_WEEK
-            TWO_WEEKS -> UiReminderDuration.TWO_WEEKS
-            ONE_MONTH -> UiReminderDuration.ONE_MONTH
+            ReminderDuration.ONE_WEEK -> UiReminderDuration.ONE_WEEK
+            ReminderDuration.TWO_WEEKS -> UiReminderDuration.TWO_WEEKS
+            ReminderDuration.ONE_MONTH -> UiReminderDuration.ONE_MONTH
+        }
+    }
+
+    fun UiReminderDuration.mapToDomain(): ReminderDuration {
+        return when (this) {
+            UiReminderDuration.ONE_WEEK -> ReminderDuration.ONE_WEEK
+            UiReminderDuration.TWO_WEEKS -> ReminderDuration.TWO_WEEKS
+            UiReminderDuration.ONE_MONTH -> ReminderDuration.ONE_MONTH
         }
     }
 }
